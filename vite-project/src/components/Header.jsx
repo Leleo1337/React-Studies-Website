f4478ddda1b9cc972ea9e5400682839a666f43c9
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-02-26 17:28:17 -0300

    new design

--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -1,20 +1,15 @@
 import React from "react"
 import '../index.css'
 
-import reactLogo from '../assets/react.svg'
-import viteLogo from '../assets/vite.svg'
-import tailwindLogo from '../assets/tailwindLogo.png'
+const leleo1337 = 'https://avatars.githubusercontent.com/u/168025120?v=4'
+
 
 export default function Header(){
     return(
         <header>
-            <nav className='flex gap-3 justify-around items-center bg-gray-900 p-4 drop-shadow-md'>
-                <div className="flex gap-3 items-center">
-                    <img src={reactLogo}/>
-                    <img src={viteLogo}/>
-                    <img className='w-8 h-6' src={tailwindLogo}/>
-                </div>
-                <ul className="flex gap-6">
+            <nav className='w-full fixed top-0 flex gap-6 justify-around items-center bg-gray-900 px-2 py-4 drop-shadow-md overflow-hidden'>
+                <img src={leleo1337} alt="Minha foto do github" className="w-10 rounded-full" />
+                <ul className="flex gap-4 text-md">
                     <li className="underline text-white"><a href="#">Home</a></li>
                     <li className="underline text-white"><a href="#">About</a></li>
                     <li className="underline text-white"><a href="#">Contact</a></li>
--- vite-project/src/components/Home.jsx
+++ vite-project/src/components/Home.jsx
@@ -1,19 +1,28 @@
 import React from "react"
 import '../index.css'
+import viteLogo from '../assets/vite.svg'
 import reactLogo from '../assets/reactLogo.png'
 import tailwindLogo from '../assets/tailwindLogo.png'
 
 export default function Home(){
     return(
         <main>
-            <section className='bg-gray-900 p-8'>
-                <h1 className='text-xl text-white p-4 rounded-2xl'>Estou usando Vite, React + TailwindCSS</h1>
-                <div className="flex gap-3 items-center">
+            <section className="bg-gray-900 pt-24 pb-2">
+                <h1 className='text-xl text-white text-center pb-4'>Estou usando Vite, React e TailwindCSS</h1>
+                <div className="flex gap-3 items-center justify-center ">
+                    <img className="w-10" src={viteLogo} alt="TailWindCSS Logo" />
                     <img className='w-12'src={reactLogo} alt="React Logo" />
-                    <img className="w-12" src={tailwindLogo} alt="" />
-                </div>                
-                <h1 className='py-2 text-3xl text-white font-semibold'>Fun facts about React</h1>
-                <ul className='text-white list-disc px-10 pb-4'>
+                    <img className="w-12" src={tailwindLogo} alt="TailWindCSS Logo" />
+                </div>
+            </section>
+            <section className='bg-gray-900 pt-2 pb-8 px-4 text-center'>
+                <h1 className='text-white text-3xl text-center py-4 font-semibold'>About this project</h1>
+                <p className="text-white">I'm building this website to learn react and enhance my skills in Tailwind CSS, HTML and JavaScript, following the
+                <span className="underline text-blue-300"><a href="https://scrimba.com/learn-react-c0e" target="_blank"> "Learn React" course on Scrimba</a> </span>, this website is made to apply what I've learned in the lessons.</p>
+            </section>
+            <section className='bg-gray-950 p-2'>
+                <h1 className='text-white text-3xl text-center md:text-start py-4 font-semibold'>Fun facts about React</h1>
+                <ul className='text-white list-disc ml-8 pb-8'>
                     <li>Was first released in 2013</li>
                     <li>Was originally created by Jordan Walke</li>
                     <li>Has well over 100k stars on GitHub</li>
@@ -21,16 +30,26 @@ export default function Home(){
                     <li>Powers thousands of interprise apps, including mobile apps</li>
                 </ul>
             </section>
-            <section className='bg-gray-950 p-8'>
-                <div className="flex gap-3 items-center">
-                    <img className='w-12'src={reactLogo} alt="React Logo" />
-                    <img className="w-12" src={tailwindLogo} alt="" />
-                </div>
-                <h1 className='text-3xl text-white font-semibold py-2'>Reasons I am excited to learn react</h1>
-                <ol className='list-decimal text-white px-10'>
+            <section className='bg-gray-900 p-2 py-8'>
+                <h1 className='text-3xl text-white font-semibold px-2.5'>Reasons I am excited to learn react <span><img className='w-8 mb-1.5 inline'src={reactLogo} alt="React Logo" /></span> </h1>
+                <ol className='list-decimal text-white ml-8'>
                     <li className='py-2'>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
                     <li>I am more likely to get a job as a front end developer if I know React</li>
                 </ol>
+            </section>
+            <section className="w-full bg-gray-950 m-auto px-2 pt-4 pb-12">
+                <h1 className="text-3xl text-white font-semibold text-center py-4">Course Challenges</h1>
+                <div className="flex flex-col gap-4 lg:grid lg:grid-rows-1 lg:grid-cols-3 lg:justify-items-center">
+                    <div className="w-full lg:w-[80%] flex items-center justify-center h-40 bg-gray-300">
+                        <span>Empty</span>
+                    </div>
+                    <div className="w-full lg:w-[80%] flex items-center justify-center h-40 bg-gray-300">
+                        <span>Empty</span>
+                    </div>
+                    <div className="w-full lg:w-[80%] flex items-center justify-center h-40 bg-gray-300">
+                        <span>Empty</span>
+                    </div>
+                </div>
             </section>
         </main>
     )
