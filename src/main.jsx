9d5f570cd0c958692c98e6502572f9e75519d915
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-02-26 18:54:25 -0300

    teste

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-
 const root = createRoot(document.querySelector('#root'))
-
 
 root.render(
   <StrictMode>
