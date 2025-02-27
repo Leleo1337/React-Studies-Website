02154550a27140baf0bca09a9bf02c8867edfd7c
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-02-27 16:22:46 -0300

    profile img links to github
    now if you click in the profile img you're linked to my github

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,17 @@ const leleo1337 = 'https://avatars.githubusercontent.com/u/168025120?v=4'
 export default function Header(){
     return(
         <header>
-            <nav className='w-full fixed top-0 flex gap-6 justify-around items-center bg-gray-900 px-2 py-4 drop-shadow-md overflow-hidden'>
-                <img src={leleo1337} alt="Minha foto do github" className="w-10 rounded-full" />
+            <nav className='w-full fixed top-0 flex gap-6 justify-around items-center bg-gray-900 px-2 py-4 drop-shadow-md overflow-hidden z-1'>
+                <a href="https://github.com/leleo1337" target="_blank"><img src={leleo1337} alt="Minha foto do github" className="w-10 rounded-full" /></a>
                 <ul className="flex gap-4 text-md">
                     <li className="underline text-white"><a href="Home">Home</a></li>
                     <li className="underline text-white"><a href="#">About</a></li>
                     <li className="underline text-white"><a href="#">Contact</a></li>
                 </ul>
             </nav>
+            <div className="absolute top-0 w-full h-[72px] -z-20">
+                Teste
+            </div>
         </header>
     )
 }
