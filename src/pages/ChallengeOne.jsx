c52612286646ca35246e0f49588a5df8aed102b6
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-02-27 19:38:51 -0300

    removed mb on li

--- src/pages/ChallengeOne.jsx
+++ src/pages/ChallengeOne.jsx
@@ -1,4 +1,4 @@
-import Header from "../components/header"
+import Header from "../components/Header"
 import Footer from "../components/Footer"
 
 import react from '../assets/img/reactLogo.png'
@@ -10,7 +10,7 @@ export default function ChallengeOne(){
         <>
             <Header />  {/* Page header */}
             <p className="text-3xl">oi</p>
-                <div className="container absolute top-[50%] left-[50%] translate-[-50%] w-[520px] bg-gray-800 rounded-md overflow-hidden shadow-xl">
+                <div className="absolute top-[50%] left-[50%] translate-[-50%] w-[520px] bg-gray-800 rounded-md overflow-hidden shadow-xl">
                     <header className="flex w-full bg-c1Header h-[90px] rounded-t-md"> {/* Container header */}
                         <div className="flex justify-start items-center gap-2 ml-6 text-cyan-300">
                             <img src={react} alt="REACT " className=" w-8 "/>
@@ -21,11 +21,11 @@ export default function ChallengeOne(){
                         <div>
                             <h1 className="text-white text-4xl font-inter font-bold mt-8">Fun facts about React</h1>
                             <ul className="list-disc ml-12 mt-9 marker:text-cyan-400 marker:text-2xl marker:relative marker:top-32 text-md text-gray-200">
-                                <li className="mb-2">Was first released in 2013</li>
-                                <li className="mb-2">Was originally created by Jordan Walke</li>
-                                <li className="mb-2">Has well over 100k stars on GitHub</li>
-                                <li className="mb-2">Is mainted by Meta</li>
-                                <li className="mb-2">Powers thousands of interprise apps, including mobile apps</li>
+                                <li>Was first released in 2013</li>
+                                <li>Was originally created by Jordan Walke</li>
+                                <li>Has well over 100k stars on GitHub</li>
+                                <li>Is mainted by Meta</li>
+                                <li>Powers thousands of interprise apps, including mobile apps</li>
                             </ul>
                         </div>
                         <div className="w-18"><img src={reactBg} alt="React  at the background" className="absolute bottom-18 left-97 w-[260px]"/></div>
