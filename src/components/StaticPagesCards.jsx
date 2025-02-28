411802137af8e621b3add2f0c5a0409303b8f813
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-02-28 12:32:25 -0300

    fixed card shadow

--- src/components/StaticPagesCards.jsx
+++ src/components/StaticPagesCards.jsx
@@ -1,18 +1,20 @@
+import cardShadow from '../assets/img/card-shadow.png'
+
 export default function StaticPageCards(){
     return(
         <>
             <div className="flex flex-col gap-4 lg:grid lg:grid-rows-1 lg:grid-cols-3 lg:justify-items-center px-12">
                 <a href="c1" className="card-style group bg-[url(/src/assets/img/challenges/c1Blur.png)] opacity-50 hover:opacity-70 hover:bg-[url(/src/assets/img/Challenges/c1NoBlur.png)]">
                     <span className="group-hover:underline absolute bottom-6 left-5 text-gray-50 text-2xl font-semibold z-10">Challenge One / React Facts</span>
-                    <img src="src\assets\img\image-shadow.png" alt="image shadow" className="absolute bottom-0 h-20 z-1 rounded-b-xl" />
+                    <img src={cardShadow} alt="image shadow" className="absolute bottom-0 h-20 z-1 rounded-b-xl" />
                 </a>
                 <a href="c1" className="card-style group bg-[url(/src/assets/img/challenges/c1Blur.png)]  opacity-50 hover:opacity-70 hover:bg-[url(/src/assets/img/Challenges/c1NoBlur.png)]">
                     <span className="group-hover:underline absolute bottom-6 left-5 text-white text-2xl font-semibold z-10">Challenge Two</span>
-                    <img src="src\assets\img\image-shadow.png" alt="image shadow" className="absolute bottom-0 h-20 z-1 rounded-b-xl" />
+                    <img src={cardShadow} alt="image shadow" className="absolute bottom-0 h-20 z-1 rounded-b-xl" />
                 </a>
                 <a href="c1" className="card-style group bg-[url(/src/assets/img/challenges/c1Blur.png)] opacity-50 hover:opacity-70 hover:bg-[url(/src/assets/img/Challenges/c1NoBlur.png)]">
+                    <img src={cardShadow}alt="image shadow" className="absolute bottom-0 h-20 z-1 rounded-b-xl" />
                     <span className="group-hover:underline absolute bottom-6 left-5 text-white text-2xl font-semibold z-10">Challenge Three</span>
-                    <img src="src\assets\img\image-shadow.png" alt="image shadow" className="absolute bottom-0 h-20 z-1 rounded-b-xl" />
                 </a>   
             </div>
         </>
