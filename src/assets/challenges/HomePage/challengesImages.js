e02cb13e3b1eacb9b2c4257e20473eeb1a57148f
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-03-03 14:30:26 -0300

    Made a object for storing images
    too many imports on home were giving me ticks

---
+++ src/assets/challenges/HomePage/challengesImages.js
@@ -0,0 +1,29 @@
+// exercise preview 
+
+import c1Blur from "./c1Blur.png";
+import c1NoBlur from "./c1NoBlur.png";
+import c2Blur from "./c2Blur.png";
+import c2NoBlur from "./c2NoBlur.png";
+import c3Blur from "./c3Blur.png";
+import c3NoBlur from "./c3NoBlur.png";
+
+const challengesHomeImages = {
+  c1: {
+     blur: c1Blur, 
+     noBlur: c1NoBlur 
+    },
+    c2: { 
+        blur: c2Blur, 
+        noBlur: c2NoBlur 
+    },
+    c3: { 
+        blur: c3Blur, 
+        noBlur: c3NoBlur 
+    },
+    c4: { 
+        blur: '', 
+        noBlur: '' 
+    },
+}
+
+export default challengesHomeImages;
