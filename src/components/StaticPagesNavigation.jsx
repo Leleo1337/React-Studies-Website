5eb12ab341ba35d3b2a685b23527dda4ec3f9aab
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-03-03 14:33:02 -0300

    made it responsive ( i thought it alr was)
    removed img tag , added background image on Link, now it's responsive

--- src/components/StaticPagesNavigation.jsx
+++ src/components/StaticPagesNavigation.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'; // Adicione esta importação
+import { Link } from 'react-router-dom'; // navegação SPA
 import { useState } from 'react';
 
 import cardShadow from '../assets/card-shadow.png'
@@ -7,15 +7,13 @@ export default function StaticPageCards(props){
     const [ isHovered, setIsHovered ] = useState(false)
 
     return(
-            <div className='w-full pb-8 text-3xl font-semibold text-center text-white'>
+            <div className='pb-8 text-3xl font-semibold text-center text-white sm:w-full'>
                 <h3 className='pb-2.5 text-2xl'>{props.title}</h3>
-            <Link to={props.path} className='mx-auto shadow card-style' 
-            onMouseEnter={() => setIsHovered(true)} 
-            onMouseLeave={() => setIsHovered(false)} >
-                <img src={isHovered ? props.hoverImg : props.img } alt="Challenge preview" className='w-full rounded-xl opacity-40 hover:opacity-60'/> 
-                <span className="absolute text-lg font-semibold hover:underline bottom-4 left-5 text-gray-50 z-1">
-                    {props.name}
-                </span>
+            <Link to={props.path} className='mx-auto shadow h-50 sm:h-80 opacity-55 hover:opacity-80 card-style' style={{ backgroundImage: `url(${isHovered ? props.hoverImg : props.img})` }}
+                onMouseEnter={() => setIsHovered(true)} 
+                onMouseLeave={() => setIsHovered(false)}
+            >
+                <span className="absolute text-lg font-semibold hover:underline bottom-4 left-5 text-gray-50 z-1">{props.name}</span>
                 <img src={cardShadow} alt="image shadow" className="absolute bottom-0 left-0 z-0 h-20 rounded-b-xl" />
             </Link>
             </div>
