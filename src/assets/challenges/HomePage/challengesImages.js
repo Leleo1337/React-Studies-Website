54e08e462aae6925c631083303d64dc9c2d3005b
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-03-03 18:16:42 -0300

    new images

--- src/assets/challenges/HomePage/challengesImages.js
+++ src/assets/challenges/HomePage/challengesImages.js
@@ -1,11 +1,13 @@
 // exercise preview 
 
-import c1Blur from "./c1Blur.png";
-import c1NoBlur from "./c1NoBlur.png";
-import c2Blur from "./c2Blur.png";
-import c2NoBlur from "./c2NoBlur.png";
-import c3Blur from "./c3Blur.png";
-import c3NoBlur from "./c3NoBlur.png";
+import c1Blur from "./c1Blur.png"
+import c1NoBlur from "./c1NoBlur.png"
+import c2Blur from "./c2Blur.png"
+import c2NoBlur from "./c2NoBlur.png"
+import c3Blur from "./c3Blur.png"
+import c3NoBlur from "./c3NoBlur.png"
+import c4Blur from "./c4Blur.png"
+import c4NoBlur from "./c4NoBlur.png"
 
 const challengesHomeImages = {
   c1: {
@@ -21,6 +23,10 @@ const challengesHomeImages = {
         noBlur: c3NoBlur 
     },
     c4: { 
+        blur: c4Blur ,
+        noBlur: c4NoBlur
+    },
+    c5: { 
         blur: '', 
         noBlur: '' 
     },
