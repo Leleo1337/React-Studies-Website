856dd6b3c25101834a4ffcba52d4edfa7e8b4562
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-03-03 18:16:04 -0300

    added some opacity for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer(){
     return (
-        <footer className="fixed bottom-0 w-full p-2 text-center text-white bg-black z-2">
-            <small>© 2024 <a href="https://github.com/leleo1337" target="_blank" className="underline text-blue-300 text-base px-0.5 hover:text-blue-500">Leo</a> development. All rights reserved.</small>
+        <footer className="fixed bottom-0 w-full p-2 text-center text-white bg-black opacity-95 z-2">
+            <small>© 2025 <a href="https://github.com/leleo1337" target="_blank" className="underline text-blue-300 text-base px-0.5 hover:text-blue-500">Leo</a> development. All rights reserved.</small>
         </footer>
     )
 }
