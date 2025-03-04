7ea30a5c3a453ed3c16cc67f92a3fc360e83b440
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-03-04 17:38:02 -0300

    router

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,15 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './ScrollToTop.js';
 import './index.css' // tailwind css
 
-import BrowserRoutes from './BrowserRoutes.jsx'
+import Home from './pages/Home.jsx';
+import ChallengeOne from './pages/challenges/Challengeone.jsx';
+import ChallengeTwo from './pages/challenges/ChallengeTwo.jsx';
+import ChallengeThree from './pages/challenges/ChallengeThree.jsx';
+import ChallengeFour from './pages/challenges/ChallengeFour.jsx';
 
 const root = createRoot(document.querySelector('#root'))
-
 root.render(
   <StrictMode>
-      <RouterProvider router={BrowserRoutes} />
+    <BrowserRouter basename='/reactjs'>
+      <ScrollToTop />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/c1" element={<ChallengeOne />} />
+            <Route path="/c2" element={<ChallengeTwo />} />
+            <Route path="/c3" element={<ChallengeThree />} />
+            <Route path="/c4" element={<ChallengeFour />} />
+        </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
