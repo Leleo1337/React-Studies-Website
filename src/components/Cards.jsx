2e1014590df56a262d2803396f04daad8c066b87
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-03-05 02:43:51 -0300

    Renamed to cards

---
+++ src/components/Cards.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom'; // navegação SPA
+import { useState } from 'react';
+
+export default function ChallengeCard(props){
+    const [ isHovered, setIsHovered ] = useState(false)
+
+    return(
+        <div className='items-center w-full h-70 md:h-75 bg-gradient-to-br from-white/60 via-gray-700/40 to-black/20 rounded-md shadow mb-4'>
+            <div className='w-full h-35 md:h-43 md:hover:h-48 overflow-hidden rounded-t-sm bg-left-top transition-all ease-in duration-200' >
+                <Link to={props.data.path} className='bg-black overflow-hidden opacity-85 hover:blur-sm'>
+                    <div className='w-full h-full transition ease-in duration-200 hover:scale-105' style={{ backgroundImage: `url(${props.data.img})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}></div>
+                </Link>
+            </div>
+            <div className='h-32 p-1'>
+                <h1 className='mt-2 text-center text-white font-semibold text-xl'>{props.data.title}</h1>
+                <p className='text-white break-words pt-4 ml-4 font-inter'>{props.data.summary}</p>
+            </div>
+        </div>
+    )
+}
