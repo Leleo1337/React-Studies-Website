2c1295dc414cc13aa1c8344baa927bb75d2e730b
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-03-04 21:57:44 -0300

    new design for Home

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer(){
     return (
-        <footer className="fixed bottom-0 w-full p-2 text-center text-white bg-black opacity-95 z-2">
+        <footer className="fixed bottom-0 w-full p-2 text-center text-white bg-black z-2">
             <small>© 2025 <a href="https://github.com/leleo1337" target="_blank" className="underline text-blue-300 text-base px-0.5 hover:text-blue-500">Leo</a> development. All rights reserved.</small>
         </footer>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Header from '../components/Header'
 import Footer from '../components/Footer';
+
 import ChallengeCard from "../components/ChallengesNavigation.jsx"
-import homeData from './homeData.js'
+import cardData from './cardData.js'
 
 import viteLogo from '../assets/vite.svg'
 import reactLogo from '../assets/reactLogo.png'
 import tailwindLogo from '../assets/tailwindLogo.png'
+import bgImage from '../assets/bgImage.webp'
 
 export default function Home(){
     // gambiarra :p
@@ -13,7 +15,7 @@ export default function Home(){
     document.body.classList.remove('bg-gray-100')
     document.body.classList.add('bg-mainBg')
     
-    const challengeCards = homeData.map(data =>{
+    const challengeCards = cardData.map(data =>{
         return <ChallengeCard data={data}/>
     })
 
@@ -21,24 +23,18 @@ export default function Home(){
         <>
             <Header />
             <main>
-                <section className='px-4 pt-8 pb-8 text-center bg-gray-900'>
+                <section className='flex flex-col items-center justify-center flex-nowrap shrink h-[78vh] px-4 pt-8 pb-8 bg-black opacity-99'>
+                    <div className='absolute t-0 w-full h-[78vh] -z-3'><img src={bgImage} className='w-full h-full opacity-5' /></div>
+                    <img src="https://avatars.githubusercontent.com/u/168025120?v=4" alt="Leleo1337 github image" className='rounded-full w-40 mb-8' />
                     <div className="flex items-center justify-center gap-3 ">
                         <img className="w-10" src={viteLogo} alt="TailWindCSS Logo" />
                         <img className='w-12'src={reactLogo} alt="React Logo" />
                         <img className="w-12" src={tailwindLogo} alt="TailWindCSS Logo" />
                     </div>
-                    <h1 className='py-4 text-5xl font-semibold text-center text-white'>About this project</h1>
-                    <p className="text-white">I'm building this website to learn react and enhance my skills in Tailwind CSS, HTML and JavaScript, following the
-                    <span className="text-blue-300 underline"><a href="https://scrimba.com/learn-react-c0e" target="_blank"> "Learn React" course on Scrimba</a> </span>, this website is made to apply what I've learned in the lessons.</p>
+                    <h1 className='pb-12 text-5xl font-semibold text-center text-white'>About this project</h1>
+                    <p className="text-white text-center text-">I'm building this website to learn react and enhance my skills in Tailwind CSS, HTML and JavaScript, following the <span className="text-blue-300 underline"><a href="https://scrimba.com/learn-react-c0e" target="_blank">"Learn React" course on Scrimba</a> </span>, this website is made to apply what I've learned in the lessons.</p>
                 </section>
-                <section className='p-2 py-8 bg-gray-900'>
-                    <h2 className='text-4xl text-white font-semibold px-2.5 text-center'>Reasons I am excited to learn react <span><img className='w-8 mb-1.5 inline'src={reactLogo} alt="React Logo" /></span> </h2>
-                    <ol className='pt-2 mx-8 text-white list-decimal md:w-120 md:m-auto'>
-                        <li className='py-2'>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
-                        <li>I am more likely to get a job as a front end developer if I know React</li>
-                    </ol>
-                </section>
-                <section className="w-full px-2 pt-4 pb-20 m-auto bg-gray-950">
+                <section className="w-full px-2 pt-4 pb-20 m-auto bg-zinc-950">
                     <h1 className="py-8 text-3xl font-bold text-center text-white">Course Exercises / Challenges</h1>
                     <div className="pb-8 border-b border-gray-900">
                         <div className="flex flex-col  gap-4 px-4 sm:grid sm:grid-rows-1 sm: sm:grid-cols-2 sm:justify-items-center lg:grid-cols-3 lg:justify-items-center">
