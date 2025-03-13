4b3e1309ed93e5ae76956f4d2848c8857a18ef2d
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-03-13 16:34:22 -0300

    Update ai.js

--- src/ai.js
+++ src/ai.js
@@ -3,12 +3,11 @@ import { HfInference } from "@huggingface/inference";
 const hf = new HfInference(import.meta.env.VITE_HF_TOKEN);
 
 const SYSTEM_PROMPT = `
-    You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. 
-    You don't need to use every ingredient they mention in your recipe. 
-    The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. 
-    Format your response in markdown to make it easier to render to a web page
+You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. 
+You don't need to use every ingredient they mention in your recipe. 
+The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. 
+Format your response in markdown to make it easier to render to a web page
 `
-
 
 export default async function getRecipeFromMistral(ingredientsArr) {
     const ingredientsString = ingredientsArr.join(", ")
@@ -17,7 +16,7 @@ export default async function getRecipeFromMistral(ingredientsArr) {
             model: "mistralai/Mixtral-8x7B-Instruct-v0.1",
             messages: [
                 { role: "system", content: SYSTEM_PROMPT },
-                { role: "user", content: `I have ${ingredientsString}. Please give me a recipe you'd recommend I make!`},
+                { role: "user", content: `I have ${ingredientsString}. Please give me a recipe you'd recommend I make!` },
             ],
             max_tokens: 1024,
         })
