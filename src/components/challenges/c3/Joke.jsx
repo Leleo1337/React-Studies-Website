20f7b623490c58ecdf5670c8b8fc979b53a09c34
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-03-18 14:44:43 -0300

    changed button colors

--- src/components/challenges/c3/Joke.jsx
+++ src/components/challenges/c3/Joke.jsx
@@ -13,7 +13,7 @@ export default function Joke(props){
             <div className="pb-4 text-lg">
                 {props.setup && <p className="font-semibold">{props.setup}</p>}
                 {isShown && <p className="ml-4">{props.punchline}</p> }
-                <button onClick={displayPunchLine} className="my-2 bg-gray-400 rounded-sm cursor-pointer px-2 hover:bg-gray-500 text-white">show punchline</button>
+                <button onClick={displayPunchLine} className="my-2 bg-gray-200 rounded-sm cursor-pointer px-2 hover:bg-gray-300 hover:text-white">show punchline</button>
                 <hr />
             </div>
         </>
